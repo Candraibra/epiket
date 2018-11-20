ba76cf63c509eef16ff63e157395951b82e51af8
candraibra <canisanie@gmail.com>
2018-11-21 06:13:36 +0700

    tadi error:v

--- application/views/dashboard/admin.php
+++ application/views/dashboard/admin.php
@@ -13,9 +13,9 @@
   <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
   <!-- CSS Files -->
-  <link href="<?= base_url(" assets/css/material-dashboard.css?v=2.1.0")?>" rel="stylesheet" />
+  <link href="<?= base_url("assets/css/material-dashboard.css?v=2.1.0")?>" rel="stylesheet" />
   <!-- CSS Just for demo purpose, don't include it in your project -->
-  <link href="<?= base_url(" assets/demo/demo.css")?>" rel="stylesheet" />
+  <link href="<?= base_url("assets/demo/demo.css")?>" rel="stylesheet" />
 </head>
 
 <body class="">
@@ -175,16 +175,16 @@
       </div>
     </div>
     <!--   Core JS Files   -->
-    <script src="<?= base_url(" assets/js/core/jquery.min.js")?>" type="text / javascript"></script>
-    <script src="<?= base_url(" assets/js/core/popper.min.js")?>" type="text / javascript"></script>
-    <script src="<?= base_url(" assets/js/core/bootstrap-material-design.min.js")?>" type="text / javascript"></script>
-    <script src="<?= base_url(" assets/js/plugins/perfect-scrollbar.jquery.min.js")?>"> </script> <!-- Google Maps
+    <script src="<?= base_url("assets/js/core/jquery.min.js")?>" type="text / javascript"></script>
+    <script src="<?= base_url("assets/js/core/popper.min.js")?>" type="text / javascript"></script>
+    <script src="<?= base_url("assets/js/core/bootstrap-material-design.min.js")?>" type="text / javascript"></script>
+    <script src="<?= base_url("assets/js/plugins/perfect-scrollbar.jquery.min.js")?>"> </script> <!-- Google Maps
       Plugin -->
-  <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
     <!-- Chartist JS -->
-    <script src="<?= base_url(" assets/js/plugins/chartist.min.js")?>"> </script> <!-- Notifications Plugin -->
-  <script src="<?= base_url("assets/js/plugins/bootstrap-notify.js")?>"></script>
+    <script src="<?= base_url("assets/js/plugins/chartist.min.js")?>"> </script> <!-- Notifications Plugin -->
+    <script src="<?= base_url("assets/js/plugins/bootstrap-notify.js")?>"></script>
     <!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
-    <script src="<?= base_url(" assets/js/material-dashboard.min.js?v=2.1.0")?>" type="text / javascript"></script>
+    <script src="<?= base_url("assets/js/material-dashboard.min.js?v=2.1.0")?>" type="text / javascript"></script>
     <!-- Material Dashboard DEMO methods, don't include it in your project! -->
-    <script src="<?= base_url(" assets/demo/demo.js")?>"> </script> </body> </html>
+    <script src="<?= base_url("assets/demo/demo.js")?>"> </script> </body> </html>
--- application/views/dashboard/dashboard.php
+++ application/views/dashboard/dashboard.php
@@ -13,9 +13,9 @@
   <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
   <!-- CSS Files -->
-  <link href="<?= base_url(" assets/css/material-dashboard.css?v=2.1.0")?>" rel="stylesheet" />
+  <link href="<?= base_url("assets/css/material-dashboard.css?v=2.1.0")?>" rel="stylesheet" />
   <!-- CSS Just for demo purpose, don't include it in your project -->
-  <link href="<?= base_url(" assets/demo/demo.css")?>" rel="stylesheet" />
+  <link href="<?= base_url("assets/demo/demo.css")?>" rel="stylesheet" />
 </head>
 
 <body class="">
